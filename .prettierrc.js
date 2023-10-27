26f2a6c942fabac573de559386674f196831d04c
Alonso8729 <alon8729@gmail.com>
2023-10-27 20:28:38 +0300

    Added html rule

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-  "printWidth": 80,
-  "editor.formatOnSave": true,
-  "proseWrap": "always",
-  "tabWidth": 4,
-  "requireConfig": false,
-  "useTabs": false,
-  "trailingComma": "none",
-  "bracketSpacing": true,
-  "jsxBracketSameLine": false,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    printWidth: 80,
+    'editor.formatOnSave': true,
+    proseWrap: 'always',
+    tabWidth: 4,
+    requireConfig: false,
+    useTabs: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    'editor.formatOnSave': true,
+    'editor.formatOnPaste': true,
+    'editor.formatOnType': true,
+    'editor.defaultFormatter': 'esbenp.prettier-vscode',
+    '[html]': {
+        'editor.defaultFormatter': 'vscode.html-language-features'
+    }
 }
