41b9e24c9bab171f5819c350d9f40cf06e583800
Alonso8729 <alon8729@gmail.com>
2023-10-27 20:23:37 +0300

    Added rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  tabWidth: 2,
+  tabWidth: 4,
   semi: false,
   singleQuote: true,
+  "printWidth": 80,
+  "editor.formatOnSave": true,
+  "proseWrap": "always",
+  "tabWidth": 4,
+  "requireConfig": false,
+  "useTabs": false,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
 }
