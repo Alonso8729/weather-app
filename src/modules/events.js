f7c38c208079f2f2d88ed7d3d5901a89c228bc78
Alonso8729 <alon8729@gmail.com>
2023-10-28 11:06:11 +0300

    Changed variables scope

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,44 +1,44 @@
-import dom from './dom.js';
-import weather from './weather.js';
+import dom from './dom.js'
+import weather from './weather.js'
 
 const events = (() => {
-
     function clickListener() {
-        const searchBox = document.getElementById('search-box');
-        const nav = document.querySelector('.nav');
+        const searchBox = document.getElementById('search-box')
+        const nav = document.querySelector('.nav')
+        let input
+        let unitType
 
         nav.addEventListener('click', async (event) => {
-            const { target } = event;
-            let input = searchBox.value;
-            let unitType
+            const { target } = event
 
             if (target.getAttribute('id') === 'search-icon') {
-                event.preventDefault();
-                load(input, unitType);
+                event.preventDefault()
+                input = searchBox.value
+                load(input, unitType)
+                console.log(input, unitType)
+            } else if (target.classList.contains('metric-btn')) {
+                unitType = 'metric'
+                load(input, unitType)
+                console.log(input, unitType)
+            } else if (target.classList.contains('imperial-btn')) {
+                unitType = 'imperial'
+                load(input, unitType)
+                console.log(input, unitType)
             }
-            else if (target.classList.contains('metric-btn')) {
-                unitType = "metric";
-                load(input, unitType);
-            }
-            else if (target.classList.contains('imperial-btn')) {
-                unitType = "imperial";
-                load(input, unitType);
-            }
-
         })
     }
 
     async function load(input = 'haifa', units = 'metric') {
-        dom.toggleLoader('loading');
-        const data = await weather.getLocationData(input, units);
-        dom.handleWeatherData(data, units);
-        dom.toggleLoader('done');
+        dom.toggleLoader('loading')
+        const data = await weather.getLocationData(input, units)
+        dom.handleWeatherData(data, units)
+        dom.toggleLoader('done')
     }
 
     return {
-        clickListener, load
-    };
+        clickListener,
+        load
+    }
+})()
 
-})();
-
-export default events;
+export default events
